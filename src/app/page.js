217d3317eaebe9cfe01d44a09adddcecbbf2b667
Arnav Agarwal <arnav22agarwal@gmail.com>
2025-04-04 22:34:34 +0530

    the cursor effect done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
-
+import { SplashCursor } from "@/components/ui/splash-cursor";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <SplashCursor 
+        BACK_COLOR={{ r: 0, g: 0, b: 0 }}
+        SPLAT_FORCE={8000}
+        COLOR_UPDATE_SPEED={15}
+      />
       </main>
     </div>
   );
